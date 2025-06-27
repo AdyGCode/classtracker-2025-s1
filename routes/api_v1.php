e177de854da5ab577ce6060b6963e01e35da7dbc
ninihub1 <myowngithubacc@gmail.com>
2025-06-27 18:20:18 +0800

    fix: Protected API routes with auth:sanctum middleware

--- routes/api_v1.php
+++ routes/api_v1.php
@@ -27,67 +27,70 @@ use Illuminate\Support\Facades\Route;
 
 //Route::post('/register', [AuthApiController::class, 'register']);
 Route::post('/login', [AuthApiController::class, 'login']);
+
+Route::middleware('auth:sanctum')->group(function () {
 //Route::get('/profile', [AuthApiController::class, 'profile']);
 //Route::post('/logout', [AuthApiController::class, 'logout']);
 
-Route::apiResource('courses', CourseApiController::class)
-    ->names([
-        'index' => 'api.v1.courses.index',
-        'show' => 'api.v1.courses.show',
-        'store' => 'api.v1.courses.store',
-        'update' => 'api.v1.courses.update',
-        'destroy' => 'api.v1.courses.destroy',
-    ]);
-
-Route::apiResource('clusters', ClusterApiController::class)
-    ->names([
-        'index' => 'api.v1.clusters.index',
-        'show' => 'api.v1.clusters.show',
-        'store' => 'api.v1.clusters.store',
-        'update' => 'api.v1.clusters.update',
-        'destroy' => 'api.v1.clusters.destroy',
-    ]);
-
-Route::apiResource('units', UnitApiController::class)
-    ->names([
-        'index' => 'api.v1.units.index',
-        'show' => 'api.v1.units.show',
-        'store' => 'api.v1.units.store',
-        'update' => 'api.v1.units.update',
-    ]);
-
-Route::apiResource('packages', PackageApiController::class)
-    ->names([
-        'index' => 'api.v1.packages.index',
-        'show' => 'api.v1.packages.show',
-        'store' => 'api.v1.packages.store',
-        'update' => 'api.v1.packages.update',
-        'destroy' => 'api.v1.packages.destroy',
-    ]);
-
-Route::apiResource('lessons', LessonApiController::class)
-    ->names([
-        'index' => 'api.v1.lessons.index',
-        'show' => 'api.v1.lessons.show',
-        'store' => 'api.v1.lessons.store',
-        'update' => 'api.v1.lessons.update',
-        'destroy' => 'api.v1.lessons.destroy',
-    ]);
-
-Route::prefix('roles-permissions')->group(function () {
-    Route::get('/', [RolesAndPermissionsApiController::class, 'index']);
-    Route::post('/assign', [RolesAndPermissionsApiController::class, 'assignRole']);
-    Route::delete('/remove', [RolesAndPermissionsApiController::class, 'removeRole']);
-    Route::get('/user/{user}', [RolesAndPermissionsApiController::class, 'getUserRoles']);
-});
-
-Route::name('api.v1.')->group(function () {
-    Route::apiResource('users', UserApiController::class)
+    Route::apiResource('courses', CourseApiController::class)
         ->names([
-            'index' => 'users.index',
-            'store' => 'users.store',
-            'show' => 'users.show',
-            'update' => 'users.update',
-            'destroy' => 'users.destroy',
+            'index' => 'api.v1.courses.index',
+            'show' => 'api.v1.courses.show',
+            'store' => 'api.v1.courses.store',
+            'update' => 'api.v1.courses.update',
+            'destroy' => 'api.v1.courses.destroy',
         ]);
+
+    Route::apiResource('clusters', ClusterApiController::class)
+        ->names([
+            'index' => 'api.v1.clusters.index',
+            'show' => 'api.v1.clusters.show',
+            'store' => 'api.v1.clusters.store',
+            'update' => 'api.v1.clusters.update',
+            'destroy' => 'api.v1.clusters.destroy',
+        ]);
+
+    Route::apiResource('units', UnitApiController::class)
+        ->names([
+            'index' => 'api.v1.units.index',
+            'show' => 'api.v1.units.show',
+            'store' => 'api.v1.units.store',
+            'update' => 'api.v1.units.update',
+        ]);
+
+    Route::apiResource('packages', PackageApiController::class)
+        ->names([
+            'index' => 'api.v1.packages.index',
+            'show' => 'api.v1.packages.show',
+            'store' => 'api.v1.packages.store',
+            'update' => 'api.v1.packages.update',
+            'destroy' => 'api.v1.packages.destroy',
+        ]);
+
+    Route::apiResource('lessons', LessonApiController::class)
+        ->names([
+            'index' => 'api.v1.lessons.index',
+            'show' => 'api.v1.lessons.show',
+            'store' => 'api.v1.lessons.store',
+            'update' => 'api.v1.lessons.update',
+            'destroy' => 'api.v1.lessons.destroy',
+        ]);
+
+    Route::prefix('roles-permissions')->group(function () {
+        Route::get('/', [RolesAndPermissionsApiController::class, 'index']);
+        Route::post('/assign', [RolesAndPermissionsApiController::class, 'assignRole']);
+        Route::delete('/remove', [RolesAndPermissionsApiController::class, 'removeRole']);
+        Route::get('/user/{user}', [RolesAndPermissionsApiController::class, 'getUserRoles']);
+    });
+
+    Route::name('api.v1.')->group(function () {
+        Route::apiResource('users', UserApiController::class)
+            ->names([
+                'index' => 'users.index',
+                'store' => 'users.store',
+                'show' => 'users.show',
+                'update' => 'users.update',
+                'destroy' => 'users.destroy',
+            ]);
+    });
 });
